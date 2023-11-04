0a594edc4a23378909ab7b8b356ab1d9bc92f1c1
dev-lovi <santiagolovisotto@gmail.com>
2023-11-04 16:01:34 -0300

    run

--- main.py
+++ main.py
@@ -1,38 +1,18 @@
-#Starting from zero
-from kivy.lang import Builder
 from kivymd.app import MDApp
-from kivy.uix.widget import Widget
-from kivymd.uix.card import MDCard
-from kivymd.uix.screen import MDScreen
-from kivy.uix.screenmanager import ScreenManager
-from kivy.config import Config
-from kivy.core.window import Window
-from kivy.core.text import LabelBase
-from kivy.clock import Clock
-from kivy.core.audio import SoundLoader
+from kivy.lang import Builder
 
-
-
-LabelBase.register(name="Poppins", fn_regular="Poppins-Bold.ttf")
-Window.size = (360, 640)
-
-Config.set('graphics', 'width', '360')
-Config.set('graphics', 'height', '740')
-
-class cls(MDApp):
+class SampleApp(MDApp):
+    
     def build(self):
-        self.theme_cls.theme_style = 'Dark'
-        self.theme_cls.primary_palette = 'Teal'
-        kv = Builder.load_file('cls.kv')
-        return kv
+        self.appKv='''
+MDScreen:
+    MDLabel:
+        text:'Hello,World.'
+        multiline:True
+        halign:'center'         
+'''
+        AppScreen=Builder.load_string(self.appKv)
+        return AppScreen
 
-
-class WindowManager(ScreenManager):
-    pass
-
-
-
+SampleApp().run()
     
-    
-if __name__ == '__main__':
-    cls().run()
